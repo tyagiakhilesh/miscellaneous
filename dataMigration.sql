a4736c117cee204222661d2812b9b7f17619da13
Akhilesh Tyagi <tyagiakhilesh@gmail.com>
2015-04-08 02:07:13 +0530

    Update dataMigration.sql
    Correcting a significant flaw in the script.

--- dataMigration.sql
+++ dataMigration.sql
@@ -1,3 +1,6 @@
+DROP FUNCTION IF EXISTS blob_write(bytea);
+DROP FUNCTION IF EXISTS migratedata(text[]);
+
 -- This function is for creating a blob in pg_largeobject table for supplied input
 -- Given an input, it shall create a entry in aforementioned table and shall return
 -- the OID.
@@ -21,41 +24,64 @@ $func$  LANGUAGE plpgsql VOLATILE STRICT;
 -- shall insert its data into pg_largeobject table, shall place oid value
 -- into the temp column. And once its done with the data, it shall drop the
 -- original column, and rename the temp column as original column
-CREATE OR REPLACE FUNCTION migrateData(VARIADIC text[]) RETURNS VOID AS
+CREATE OR REPLACE FUNCTION migratedata(VARIADIC text[]) RETURNS VOID AS
 $BODY$
 DECLARE
-    aRecord RECORD;
-    bRecord RECORD;
+    arecord RECORD;
+    brecord RECORD;
     _tbl text;
     _col text;
     _col_tmp text;
     _oid int;
     _record_col text;
+    _update_query text;
+    _value_json json;
 BEGIN
+
+    RAISE INFO '';
+    RAISE INFO '';
+    RAISE INFO 'Dropping aun_language_type table as its structure has been changed. Don''t worry, its shall not harm you';
+    
+    DROP TABLE IF EXISTS aun_language_type;
+
+    RAISE INFO 'Drop complete.';
+
+    RAISE INFO '';
+    RAISE INFO '';
     RAISE INFO 'Starting migration for % tables',$1;
     
-    FOR aRecord IN
+    FOR arecord IN
 	SELECT table_name,column_name FROM information_schema.columns 
 		WHERE data_type='text' and table_name = ANY ($1)
     LOOP
-	RAISE INFO 'Starting migration for table %  for column %',aRecord.table_name,aRecord.column_name;
-	_tbl := aRecord.table_name;
-	_col := aRecord.column_name;
+	RAISE INFO 'Starting migration for table %  for column %',arecord.table_name,arecord.column_name;
+	_tbl := arecord.table_name;
+	_col := arecord.column_name;
 	-- append _tmp to column name
 	_col_tmp := _col || '_tmp';
 
 	EXECUTE format('ALTER TABLE %I ADD COLUMN %I text',_tbl,_col_tmp);
 
-	FOR bRecord IN
+	FOR brecord IN
 		EXECUTE format('SELECT * FROM %I',_tbl)
 	LOOP
-		_record_col := format('bRecord.%I',_col);
-				
+		-- extract the columns into the json
+		_value_json = row_to_json(brecord);
+		--RAISE INFO 'json is %', _value_json;
+		_record_col = format('%L',_value_json->>_col);
+
+		RAISE INFO 'Value going to be replaced is %', _record_col;
+
 		EXECUTE 'SELECT blob_write(decode( $1,''escape''))' INTO _oid USING _record_col;
 
-		--RAISE INFO 'GENERATED OID value is %', _oid;
+		RAISE INFO 'GENERATED OID is % for text %', _oid, _record_col;
 		
-		EXECUTE format('UPDATE %I SET %I = %s WHERE %I=%s' , _tbl, _col_tmp,_oid,_col,_col,_record_col);
+		_update_query := format('UPDATE %I SET %I = %s WHERE %I=%s' , _tbl, _col_tmp,_oid,_col,_record_col);
+
+		RAISE INFO 'Update Query is %',_update_query;
+		
+		EXECUTE _update_query;
+		
 	END LOOP;
 
 	EXECUTE format('ALTER TABLE %I DROP COLUMN %I',_tbl,_col);
@@ -67,6 +93,5 @@ BEGIN
 END
 $BODY$
 LANGUAGE plpgsql;
-
--- KINDLY PROVIDE THE TABLE NAMES for which you want to do the upgrade.
-SELECT migrateData('tablename1', 'tablename2');
+-- provide tables names as below
+SELECT migratedata('tablename1', 'tablename2');
